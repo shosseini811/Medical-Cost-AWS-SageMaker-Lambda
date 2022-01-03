9957337b4781c3dd712f574599f496c94f376003
Sohail Hosseini <S.SADATHOSSEINI@GMAIL.COM>
2022-01-03 15:50:09 -0600

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,32 @@
+import os
+import io
+import boto3
+import json
+import csv
+
+# grab environment variables
+ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
+runtime= boto3.client('runtime.sagemaker')
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+    
+    data = json.loads(json.dumps(event))
+    print(data)
+    payload = data['data']
+    print(payload)
+    
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+                                      ContentType='text/csv',
+                                      Body=payload)
+    print(response)
+    result = json.loads(response['Body'].read().decode())
+    print(result)
+    
+    pred = float(result['predictions'][0]['score'])
+    
+    return str(pred)
+
+
+
+    
